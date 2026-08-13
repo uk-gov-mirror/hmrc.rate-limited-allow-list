hmrc/rate-limited-allow-list

Added new AllowListConfiguration, timeframe and percentage rate limits

app/uk/gov/hmrc/ratelimitedallowlist/repositories/AllowListRepository.scala
@@ -17,32 +17,38 @@
 package uk.gov.hmrc.ratelimitedallowlist.repositories
 
 import com.mongodb.MongoException
-import uk.gov.hmrc.ratelimitedallowlist.models.Done
-import uk.gov.hmrc.ratelimitedallowlist.models.domain.{AllowListEntry, Feature, Service}
-import uk.gov.hmrc.ratelimitedallowlist.models.domain.AllowListEntry.Field
+import org.mongodb.scala.bson.conversions.Bson
 import org.mongodb.scala.model.*
 import play.api.Configuration
+import play.api.libs.json.{JsValue, Json, OFormat}
 import uk.gov.hmrc.crypto.{Hasher, PlainText}
 import uk.gov.hmrc.mongo.MongoComponent
 import uk.gov.hmrc.mongo.play.json.PlayMongoRepository
+import uk.gov.hmrc.ratelimitedallowlist.models.Done
+import uk.gov.hmrc.ratelimitedallowlist.models.domain.AllowListEntry.Field
+import uk.gov.hmrc.ratelimitedallowlist.models.domain.*
 
-import java.time.Clock
+import java.time.Instant
 import java.util.concurrent.TimeUnit
 import javax.inject.{Inject, Singleton}
 import scala.concurrent.{ExecutionContext, Future}
 
-trait AllowListRepository {
+case class Res(count: Int, usersInTimeframe: Int)
+object Res:
+  implicit val format: OFormat[Res] = Json.format[Res]
+
+trait AllowListRepository:
   def set(service: Service, feature: Feature, value: String): Future[Done]
   def clear(service: Service, feature: Feature): Future[Done]
   def checkExists(service: Service, feature: Feature, value: String): Future[Boolean]
   def count(service: Service, feature: Feature): Future[Long]
-}
+  def countWithinTimeframe(config: AllowListConfiguration): Future[(Long, Long)]
 
 @Singleton
 class AllowListRepositoryImpl @Inject()(mongoComponent: MongoComponent,
                                         config: Configuration,
                                         hasher: Hasher,
-                                        clock: Clock
+                                        clock: UkTime
                                    )(using ExecutionContext)
   extends PlayMongoRepository[AllowListEntry](
     collectionName = "allow-list",
@@ -63,30 +69,29 @@ class AllowListRepositoryImpl @Inject()(mongoComponent: MongoComponent,
           .unique(true)
       )
     )
-  ) with AllowListRepository {
+  ) with AllowListRepository:
 
-  def set(service: Service, feature: Feature, value: String): Future[Done] = {
+  override def set(service: Service, feature: Feature, value: String): Future[Done] =
     val hashedValue = hasher.hash(PlainText(value)).value
-    val entry = AllowListEntry(service.value, feature.value, hashedValue, clock.instant())
+    val entry = AllowListEntry(service.value, feature.value, hashedValue, clock.now)
 
     collection
       .insertOne(entry)
       .toFuture()
       .map(_ => Done)
-      .recover {
+      .recover:
         case e: MongoException if e.getCode == DuplicateErrorCode => Done
-      }
-  }
 
-  def clear(service: Service, feature: Feature): Future[Done] =
+  override def clear(service: Service, feature: Feature): Future[Done] =
     collection
       .deleteMany(Filters.and(
         Filters.equal(Field.service, service.value),
         Filters.equal(Field.feature, feature.value)
-      )).toFuture()
+      ))
+      .toFuture()
       .map(_ => Done)
 
-  def checkExists(service: Service, feature: Feature, value: String): Future[Boolean] = {
+  override def checkExists(service: Service, feature: Feature, value: String): Future[Boolean] =
     val hashedValue = hasher.hash(PlainText(value)).value
     collection
       .find(Filters.and(
@@ -96,12 +101,36 @@ class AllowListRepositoryImpl @Inject()(mongoComponent: MongoComponent,
       ))
       .toFuture()
       .map(_.nonEmpty)
-  }
 
-  def count(service: Service, feature: Feature): Future[Long] =
+  override def count(service: Service, feature: Feature): Future[Long] =
     collection.countDocuments(Filters.and(
       Filters.equal(Field.service, service.value),
       Filters.equal(Field.feature, feature.value)
     )).toFuture()
-}
 
+  def countWithinTimeframe(config: AllowListConfiguration): Future[(Long, Long)] =
+    val totalQuery = collection.countDocuments(Filters.and(
+      Filters.equal(Field.service, config.service),
+      Filters.equal(Field.feature, config.feature)
+    )).toFuture()
+
+    val timeframeQuery = collection.countDocuments(Filters.and(
+      Filters.equal(Field.service, config.service),
+      Filters.equal(Field.feature, config.feature),
+      filterWithin(config.timeframe)
+    )).toFuture()
+
+    for
+      total <- totalQuery
+      inTimeframe <- timeframeQuery
+    yield (inTimeframe, total)
+
+  private def filterBetween(lower: Instant, upper: Instant) =
+    Filters.and(Filters.gte("created", lower), Filters.lt("created", upper))
+
+  private val filterWithin = (timeframe: Timeframe) => timeframe match
+    case Timeframe.unbounded => Filters.lte("created", clock.now)
+    case Timeframe.hourly    => filterBetween(clock.thisHour, clock.nextHour)
+    case Timeframe.daily     => filterBetween(clock.today, clock.tomorrow)
+    case Timeframe.weekly    => filterBetween(clock.startOfWeek, clock.nextWeek)
+    case Timeframe.weekdaily => filterBetween(clock.startOfWorkingWeek, clock.endOfWorkingWeek)

test/uk/gov/hmrc/ratelimitedallowlist/repositories/FakeAllowListMetadataRepository.scala
@@ -16,7 +16,7 @@
 
 package uk.gov.hmrc.ratelimitedallowlist.repositories
 
-import uk.gov.hmrc.ratelimitedallowlist.models.domain.{AllowListMetadata, Feature, Service}
+import uk.gov.hmrc.ratelimitedallowlist.models.domain.{AllowList, AllowListConfiguration, AllowListMetadata, Feature, Service}
 
 import scala.annotation.unused
 import scala.concurrent.Future
@@ -84,4 +84,44 @@ class FakeAllowListMetadataRepository(createResult: Option[CreateResult] = None,
   override def setTokens(service: Service, feature: Feature, count: Long): Future[UpdateResultResult] =
     Future.successful(setTokensResult.getOrElse(throwNotImplemented("setTokens")))
 
+}
+
+class FakeAllowListConfigurationRepository(createResult: Option[CreateResult] = None,
+                                           getResult: Option[Option[AllowListConfiguration]] = None,
+                                           getServicesResult: Option[Seq[Service]] = None,
+                                           getByServiceResult: Option[List[AllowListConfiguration]] = None,
+                                           updateResult: Option[AllowListConfiguration] = None,
+                                           clearResult: Option[DeleteResult] = None
+                                          ) extends AllowListConfigurationRepository {
+
+  def throwNotImplemented(method: String) =
+    throw new NotImplementedError(
+      s"""FakeAllowListConfigurationRepository return result not configured for method `$method`. To resolve the either:
+         |  (1) pass a value that should be returned by the implementation, or
+         |  (2) if this method should not have been invoked then check your implementation""".stripMargin
+    )
+
+  @unused
+  def create(configuration: AllowListConfiguration): Future[CreateResult] =
+    Future.successful(createResult.getOrElse(throwNotImplemented("create")))
+
+  @unused
+  override def getServices(filterBy: Seq[String]): Future[Seq[Service]] =
+    Future.successful(getServicesResult.getOrElse(throwNotImplemented("getServices")))
+
+  @unused
+  override def get(service: Service): Future[List[AllowListConfiguration]] =
+    Future.successful(getByServiceResult.getOrElse(throwNotImplemented("get")))
+
+  @unused
+  override def get(allowList: AllowList): Future[Option[AllowListConfiguration]] =
+    Future.successful(getResult.getOrElse(throwNotImplemented("get")))
+
+  @unused
+  override def updateAllowListPercentageCounters(allowList: AllowList, accepted: Boolean): Future[AllowListConfiguration] =
+    Future.successful(updateResult.getOrElse(throwNotImplemented("updateAllowListPercentageCounters")))
+
+  @unused
+  override def clear(allowList: AllowList): Future[DeleteResult] =
+    Future.successful(clearResult.getOrElse(throwNotImplemented("clear")))
 }

uk-gov-mirror/hmrc.rate-limited-allow-list

app/uk/gov/hmrc/ratelimitedallowlist/repositories/AllowListRepository.scala
@@ -17,32 +17,38 @@
 package uk.gov.hmrc.ratelimitedallowlist.repositories
 
 import com.mongodb.MongoException
-import uk.gov.hmrc.ratelimitedallowlist.models.Done
-import uk.gov.hmrc.ratelimitedallowlist.models.domain.{AllowListEntry, Feature, Service}
-import uk.gov.hmrc.ratelimitedallowlist.models.domain.AllowListEntry.Field
+import org.mongodb.scala.bson.conversions.Bson
 import org.mongodb.scala.model.*
 import play.api.Configuration
+import play.api.libs.json.{JsValue, Json, OFormat}
 import uk.gov.hmrc.crypto.{Hasher, PlainText}
 import uk.gov.hmrc.mongo.MongoComponent
 import uk.gov.hmrc.mongo.play.json.PlayMongoRepository
+import uk.gov.hmrc.ratelimitedallowlist.models.Done
+import uk.gov.hmrc.ratelimitedallowlist.models.domain.AllowListEntry.Field
+import uk.gov.hmrc.ratelimitedallowlist.models.domain.*
 
-import java.time.Clock
+import java.time.Instant
 import java.util.concurrent.TimeUnit
 import javax.inject.{Inject, Singleton}
 import scala.concurrent.{ExecutionContext, Future}
 
-trait AllowListRepository {
+case class Res(count: Int, usersInTimeframe: Int)
+object Res:
+  implicit val format: OFormat[Res] = Json.format[Res]
+
+trait AllowListRepository:
   def set(service: Service, feature: Feature, value: String): Future[Done]
   def clear(service: Service, feature: Feature): Future[Done]
   def checkExists(service: Service, feature: Feature, value: String): Future[Boolean]
   def count(service: Service, feature: Feature): Future[Long]
-}
+  def countWithinTimeframe(config: AllowListConfiguration): Future[(Long, Long)]
 
 @Singleton
 class AllowListRepositoryImpl @Inject()(mongoComponent: MongoComponent,
                                         config: Configuration,
                                         hasher: Hasher,
-                                        clock: Clock
+                                        clock: UkTime
                                    )(using ExecutionContext)
   extends PlayMongoRepository[AllowListEntry](
     collectionName = "allow-list",
@@ -63,30 +69,29 @@ class AllowListRepositoryImpl @Inject()(mongoComponent: MongoComponent,
           .unique(true)
       )
     )
-  ) with AllowListRepository {
+  ) with AllowListRepository:
 
-  def set(service: Service, feature: Feature, value: String): Future[Done] = {
+  override def set(service: Service, feature: Feature, value: String): Future[Done] =
     val hashedValue = hasher.hash(PlainText(value)).value
-    val entry = AllowListEntry(service.value, feature.value, hashedValue, clock.instant())
+    val entry = AllowListEntry(service.value, feature.value, hashedValue, clock.now)
 
     collection
       .insertOne(entry)
       .toFuture()
       .map(_ => Done)
-      .recover {
+      .recover:
         case e: MongoException if e.getCode == DuplicateErrorCode => Done
-      }
-  }
 
-  def clear(service: Service, feature: Feature): Future[Done] =
+  override def clear(service: Service, feature: Feature): Future[Done] =
     collection
       .deleteMany(Filters.and(
         Filters.equal(Field.service, service.value),
         Filters.equal(Field.feature, feature.value)
-      )).toFuture()
+      ))
+      .toFuture()
       .map(_ => Done)
 
-  def checkExists(service: Service, feature: Feature, value: String): Future[Boolean] = {
+  override def checkExists(service: Service, feature: Feature, value: String): Future[Boolean] =
     val hashedValue = hasher.hash(PlainText(value)).value
     collection
       .find(Filters.and(
@@ -96,12 +101,36 @@ class AllowListRepositoryImpl @Inject()(mongoComponent: MongoComponent,
       ))
       .toFuture()
       .map(_.nonEmpty)
-  }
 
-  def count(service: Service, feature: Feature): Future[Long] =
+  override def count(service: Service, feature: Feature): Future[Long] =
     collection.countDocuments(Filters.and(
       Filters.equal(Field.service, service.value),
       Filters.equal(Field.feature, feature.value)
     )).toFuture()
-}
 
+  def countWithinTimeframe(config: AllowListConfiguration): Future[(Long, Long)] =
+    val totalQuery = collection.countDocuments(Filters.and(
+      Filters.equal(Field.service, config.service),
+      Filters.equal(Field.feature, config.feature)
+    )).toFuture()
+
+    val timeframeQuery = collection.countDocuments(Filters.and(
+      Filters.equal(Field.service, config.service),
+      Filters.equal(Field.feature, config.feature),
+      filterWithin(config.timeframe)
+    )).toFuture()
+
+    for
+      total <- totalQuery
+      inTimeframe <- timeframeQuery
+    yield (inTimeframe, total)
+
+  private def filterBetween(lower: Instant, upper: Instant) =
+    Filters.and(Filters.gte("created", lower), Filters.lt("created", upper))
+
+  private val filterWithin = (timeframe: String) => timeframe match
+    case Unbounded.bound => Filters.lte("created", clock.now)
+    case Hourly.bound    => filterBetween(clock.thisHour, clock.nextHour)
+    case Daily.bound     => filterBetween(clock.today, clock.tomorrow)
+    case Weekly.bound    => filterBetween(clock.startOfWeek, clock.nextWeek)
+    case Weekdaily.bound => filterBetween(clock.startOfWorkingWeek, clock.endOfWorkingWeek)

test/uk/gov/hmrc/ratelimitedallowlist/repositories/FakeAllowListMetadataRepository.scala
@@ -16,7 +16,7 @@
 
 package uk.gov.hmrc.ratelimitedallowlist.repositories
 
-import uk.gov.hmrc.ratelimitedallowlist.models.domain.{AllowListMetadata, Feature, Service}
+import uk.gov.hmrc.ratelimitedallowlist.models.domain.{AllowList, AllowListConfiguration, AllowListMetadata, CreateAllowListConfigurationRequest, Feature, Service}
 
 import scala.annotation.unused
 import scala.concurrent.Future
@@ -26,14 +26,14 @@ class FakeAllowListMetadataRepository(createResult: Option[CreateResult] = None,
                                       getServicesResult: Option[Seq[Service]] = None,
                                       getByServiceResult: Option[List[AllowListMetadata]] = None,
                                       clearResult: Option[DeleteResult] = None,
-                                      addTokensResult: Option[UpdateResultResult] = None,
-                                      stopIssuingTokensResult: Option[UpdateResultResult] = None,
-                                      startIssuingTokensResult: Option[UpdateResultResult] = None,
-                                      issueTokenResult: Option[UpdateResultResult] = None,
-                                      setTokensResult: Option[UpdateResultResult] = None
+                                      addTokensResult: Option[UpdateResult] = None,
+                                      stopIssuingTokensResult: Option[UpdateResult] = None,
+                                      startIssuingTokensResult: Option[UpdateResult] = None,
+                                      issueTokenResult: Option[UpdateResult] = None,
+                                      setTokensResult: Option[UpdateResult] = None
                                      ) extends AllowListMetadataRepository {
 
-  def throwNotImplemented(method: String) =
+  def throwNotImplemented(method: String): Nothing =
     throw new NotImplementedError(
       s"""FakeAllowListRepository return result not configured for method `$method`. To resolve the either: 
          |  (1) pass a value that should be returned by the implementation, or 
@@ -65,23 +65,67 @@ class FakeAllowListMetadataRepository(createResult: Option[CreateResult] = None,
     Future.successful(clearResult.getOrElse(throwNotImplemented("clear")))
 
   @unused
-  override def addTokens(service: Service, feature: Feature, incrementCount: Long): Future[UpdateResultResult] =
+  override def addTokens(service: Service, feature: Feature, incrementCount: Long): Future[UpdateResult] =
     Future.successful(addTokensResult.getOrElse(throwNotImplemented("addTokens")))
 
   @unused
-  override def stopIssuingTokens(service: Service, feature: Feature): Future[UpdateResultResult] =
+  override def stopIssuingTokens(service: Service, feature: Feature): Future[UpdateResult] =
     Future.successful(stopIssuingTokensResult.getOrElse(throwNotImplemented("stopIssuingTokens")))
 
   @unused
-  override def startIssuingTokens(service: Service, feature: Feature): Future[UpdateResultResult] =
+  override def startIssuingTokens(service: Service, feature: Feature): Future[UpdateResult] =
     Future.successful(startIssuingTokensResult.getOrElse(throwNotImplemented("startIssuingTokens")))
 
   @unused
-  override def issueToken(service: Service, feature: Feature): Future[UpdateResultResult] =
+  override def issueToken(service: Service, feature: Feature): Future[UpdateResult] =
     Future.successful(issueTokenResult.getOrElse(throwNotImplemented("issueToken")))
 
   @unused
-  override def setTokens(service: Service, feature: Feature, count: Long): Future[UpdateResultResult] =
+  override def setTokens(service: Service, feature: Feature, count: Long): Future[UpdateResult] =
     Future.successful(setTokensResult.getOrElse(throwNotImplemented("setTokens")))
 
+}
+
+class FakeAllowListConfigurationRepository(createResult: Option[CreateResult] = None,
+                                           getResult: Option[Option[AllowListConfiguration]] = None,
+                                           getServicesResult: Option[Seq[Service]] = None,
+                                           getByServiceResult: Option[List[AllowListConfiguration]] = None,
+                                           updateResult: Option[UpdateResult] = None,
+                                           updateCounterResult: Option[AllowListConfiguration] = None,
+                                           clearResult: Option[DeleteResult] = None
+                                          ) extends AllowListConfigurationRepository {
+
+  def throwNotImplemented(method: String): Nothing =
+    throw new NotImplementedError(
+      s"""FakeAllowListConfigurationRepository return result not configured for method `$method`. To resolve the either:
+         |  (1) pass a value that should be returned by the implementation, or
+         |  (2) if this method should not have been invoked then check your implementation""".stripMargin
+    )
+
+  @unused
+  def create(service: Service, configuration: CreateAllowListConfigurationRequest): Future[CreateResult] =
+    Future.successful(createResult.getOrElse(throwNotImplemented("create")))
+
+  @unused
+  override def getServices(filterBy: Seq[String]): Future[Seq[Service]] =
+    Future.successful(getServicesResult.getOrElse(throwNotImplemented("getServices")))
+
+  @unused
+  override def get(service: Service): Future[List[AllowListConfiguration]] =
+    Future.successful(getByServiceResult.getOrElse(throwNotImplemented("getByService")))
+
+  @unused
+  override def get(allowList: AllowList): Future[Option[AllowListConfiguration]] =
+    Future.successful(getResult.getOrElse(throwNotImplemented("getByAllowList")))
+
+  @unused
+  override def updateAllowListPercentageCounters(allowList: AllowList, accepted: Boolean): Future[AllowListConfiguration] =
+    Future.successful(updateCounterResult.getOrElse(throwNotImplemented("updateAllowListPercentageCounters")))
+  @unused
+  override def patch(allowList: AllowList, update: AllowListConfiguration.Update): Future[UpdateResult] =
+    Future.successful(updateResult.getOrElse(throwNotImplemented("patchAllowListConfig")))
+
+  @unused
+  override def clear(allowList: AllowList): Future[DeleteResult] =
+    Future.successful(clearResult.getOrElse(throwNotImplemented("clear")))
 }